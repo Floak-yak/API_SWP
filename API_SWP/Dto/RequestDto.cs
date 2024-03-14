681236500a7fe45c1c48041bd6754a9945531585
Floak-yak <aidolamgidi@gmail.com>
2024-03-14 10:24:00 +0700

    fix request dto

--- API_SWP/Dto/RequestDto.cs
+++ API_SWP/Dto/RequestDto.cs
@@ -3,12 +3,13 @@
     public class RequestDto
     {
         public string RequestId { get; set; } = null!;
-        public string? HouseSType { get; set; }
+        public string HouseSType { get; set; } = null!;
         public double? Size { get; set; }
-        public string? Theme { get; set; }
-        public int Status { get; set; }
+        public string Theme { get; set; } = null!;
         public DateTime? Date { get; set; }
-        public string Description { get; set; }
+        public int Status { get; set; }
+        public string? Description { get; set; }
         public string CustomerId { get; set; } = null!;
+        public string CustomerPhone { get; set; } = null!;
     }
 }
