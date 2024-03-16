c2580515ea18b361dafeb94e2d49c3d8d6eda150
Floak-yak <aidolamgidi@gmail.com>
2024-03-16 16:36:41 +0700

    debug file

--- API_SWP/obj/Debug/net6.0/API_SWP.AssemblyInfo.cs
+++ API_SWP/obj/Debug/net6.0/API_SWP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_SWP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd2b28078295a7e51d0d299a63eec5b068f1ed5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+681236500a7fe45c1c48041bd6754a9945531585")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_SWP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_SWP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
